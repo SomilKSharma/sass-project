33d1d0fea1a53e53addb3d3aa275959d331add97
Somil Sharma <somil81@icloud.com>
2023-08-27 18:30:01 +0530

    Added comments

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,8 @@ const hamburger = document.querySelector('.menu-btn__burger')
 
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item')
+
 
 let showMenu = false
 
@@ -13,11 +15,13 @@ function toggleMenu() {
         hamburger.classList.add('open')
         nav.classList.add('open')
         menuNav.classList.add('open')
+        navItems.forEach (item => item.classList.add('open'))
         showMenu=true
     } else {
         hamburger.classList.remove('open')
         nav.classList.remove('open')
         menuNav.classList.remove('open')
+        navItems.forEach (item => item.classList.remove('open'))
         showMenu = false
     }
 }
